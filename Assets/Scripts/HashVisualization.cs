460213d15e23e6ca23f7a14c4b677975d16eeb3c
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-04-16 15:54:08 +0100

    Add support for the hash of different shapes

--- Assets/Scripts/HashVisualization.cs
+++ Assets/Scripts/HashVisualization.cs
@@ -10,8 +10,34 @@ using static Unity.Mathematics.math;
 
 public class HashVisualization : MonoBehaviour {
 
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    struct HashJob : IJobFor {
+
+        [ReadOnly]
+        public NativeArray<float3> positions;
+        
+        [WriteOnly]
+        public NativeArray<uint> hashes;
+
+        public SmallXXHash hash;
+
+        public float3x4 domainTRS;
+        
+        public void Execute(int i) {
+            float3 p = mul(domainTRS, float4(positions[i], 1f));
+
+            int u = (int)floor(p.x);
+            int v = (int)floor(p.y);
+            int w = (int)floor(p.z);
+
+            hashes[i] = hash.Eat(u).Eat(v).Eat(w);
+        }
+    }
+
     static int
         hashesId = Shader.PropertyToID("_Hashes"),
+        positionsId = Shader.PropertyToID("_Positions"),
+        normalsId = Shader.PropertyToID("_Normals"),
         configId = Shader.PropertyToID("_Config");
 
     [SerializeField]
@@ -23,15 +49,9 @@ public class HashVisualization : MonoBehaviour {
     [SerializeField, Range(1, 512)]
     int resolution = 16;
 
-    NativeArray<uint> hashes;
+    [SerializeField, Range(-0.5f, 0.5f)]
+    float displacement = 0.1f;
 
-    ComputeBuffer hashesBuffer;
-
-    MaterialPropertyBlock propertyBlock;
-
-    [SerializeField, Range(-2f, 2f)]
-    float verticalOffset = 1f;
-        
     [SerializeField]
     int seed;
 
@@ -40,61 +60,47 @@ public class HashVisualization : MonoBehaviour {
         scale = 8f
     };
 
-    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
-    struct HashJob : IJobFor {
+    NativeArray<uint> hashes;
 
-        [WriteOnly]
-        public NativeArray<uint> hashes;
+    NativeArray<float3> positions, normals;
 
-        public int resolution;
+    ComputeBuffer hashesBuffer, positionsBuffer, normalsBuffer;
 
-        public float invResolution;
+    MaterialPropertyBlock propertyBlock;
 
-        public SmallXXHash hash;
+    bool isDirty;
 
-        public float3x4 domainTRS;
-
-        public void Execute(int i) {
-            float vf = (int)floor(invResolution * i + 0.00001f);
-            float uf = invResolution * (i - resolution * vf + 0.5f) - 0.5f;
-            vf = invResolution * (vf + 0.5f) - 0.5f;
-
-            float3 p = mul(domainTRS, float4(uf, 0f, vf, 1f));
-
-            int u = (int)floor(p.x);
-            int v = (int)floor(p.y);
-            int w = (int)floor(p.z);
-
-            hashes[i] = hash.Eat(u).Eat(v).Eat(w);
-        }
-    }
+    Bounds bounds;
 
     void OnEnable() {
+        isDirty = true;
         int length = resolution * resolution;
         hashes = new NativeArray<uint>(length, Allocator.Persistent);
+        positions = new NativeArray<float3>(length, Allocator.Persistent);
+        normals = new NativeArray<float3>(length, Allocator.Persistent);
         hashesBuffer = new ComputeBuffer(length, 4);
+        positionsBuffer = new ComputeBuffer(length, 3 * 4);
+        normalsBuffer = new ComputeBuffer(length, 3 * 4);
 
-        new HashJob {
-            hashes = hashes,
-            resolution = resolution,
-            invResolution = 1f / resolution,
-            hash = SmallXXHash.Seed(seed),
-            domainTRS = domain.Matrix
-        }.ScheduleParallel(hashes.Length, resolution, default).Complete();
-    
-        hashesBuffer.SetData(hashes);
-    
         propertyBlock ??= new MaterialPropertyBlock();
         propertyBlock.SetBuffer(hashesId, hashesBuffer);
+        propertyBlock.SetBuffer(positionsId, positionsBuffer);
+        propertyBlock.SetBuffer(normalsId, normalsBuffer);
         propertyBlock.SetVector(configId, new Vector4(
-            resolution, 1f / resolution, verticalOffset / resolution
+            resolution, 1f / resolution, displacement
         ));
     }
 
     void OnDisable() {
         hashes.Dispose();
+        positions.Dispose();
+        normals.Dispose();
         hashesBuffer.Release();
+        positionsBuffer.Release();
+        normalsBuffer.Release();
         hashesBuffer = null;
+        positionsBuffer = null;
+        normalsBuffer = null;
     }
 
     void OnValidate() {
@@ -105,8 +111,37 @@ public class HashVisualization : MonoBehaviour {
     }
 
     void Update() {
+        if (isDirty || transform.hasChanged) {
+            isDirty = false;
+            transform.hasChanged = false;
+            
+            JobHandle handle = Shapes.Job.ScheduleParallel(
+                positions, normals, resolution, transform.localToWorldMatrix, default
+            );
+
+            new HashJob {
+                positions = positions,
+                hashes = hashes,
+                hash = SmallXXHash.Seed(seed),
+                domainTRS = domain.Matrix
+            }.ScheduleParallel(hashes.Length, resolution, handle).Complete();
+
+            hashesBuffer.SetData(hashes);
+            positionsBuffer.SetData(positions);
+            normalsBuffer.SetData(normals);
+
+            bounds = new Bounds(
+                transform.position,
+                float3(2f * cmax(abs(transform.lossyScale)) + displacement)
+            );
+        }
+
         Graphics.DrawMeshInstancedProcedural(
-            instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
+            instanceMesh, 0, material, 
+            new Bounds(
+                transform.position, 
+                float3(2f * cmax(abs(transform.lossyScale)) + displacement)
+            ),
             hashes.Length, propertyBlock
         );
     }
---
+++ Assets/Scripts/Shapes.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+using static Unity.Mathematics.math;
+
+public static class Shapes {
+
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    public struct Job : IJobFor {
+
+        [WriteOnly]
+        NativeArray<float3> positions, normals;
+
+        public float resolution, invResolution;
+
+        public float3x4 positionTRS;
+
+        public void Execute(int i) {
+            float2 uv;
+            uv.y = floor(invResolution * i + 0.00001f);
+            uv.x = invResolution * (i - resolution * uv.y + 0.5f) - 0.5f;
+            uv.y = invResolution * (uv.y + 0.5f) - 0.5f;
+
+            positions[i] = mul(positionTRS, float4(uv.x, 0f, uv.y, 1f));
+            normals[i] = normalize(mul(positionTRS, float4(0f, 1f, 0f, 1f)));
+        }
+
+        public static JobHandle ScheduleParallel(
+            NativeArray<float3> positions, NativeArray<float3> normals, int resolution, 
+            float4x4 trs, JobHandle dependency
+        ) {
+            return new Job {
+                positions = positions,
+                normals = normals,
+                resolution = resolution,
+                invResolution = 1f / resolution,
+                positionTRS = float3x4(trs.c0.xyz, trs.c1.xyz, trs.c2.xyz, trs.c3.xyz)
+            }.ScheduleParallel(positions.Length, resolution, dependency);
+        }
+    }
+}
